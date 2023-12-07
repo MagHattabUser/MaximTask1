75eae874ab62c5a30fef6bf5e289d4b99e34fea6
User <misimov@list.ru>
2023-12-07 19:27:51 +0500

    Сделал второе задание

--- MaximTask1/Program.cs
+++ MaximTask1/Program.cs
@@ -1,27 +1,50 @@
-﻿string inputString;
+﻿using System.Text.RegularExpressions;
+
+string inputString;
 
 inputString = Console.ReadLine();
-
-int length = inputString.Length;
-if (length % 2 == 0)
+var regex = new Regex("^[a-z]+$");
+if (!regex.IsMatch(inputString))
 {
-    string firstHalf = inputString.Substring(0, length / 2);
-    string secondHalf = inputString.Substring(length / 2);
-    firstHalf = Reverse(firstHalf);
-    secondHalf = Reverse(secondHalf);
-
-    inputString = firstHalf + secondHalf;
+    string errorMessage = "";
+    foreach (var item in inputString)
+    {
+        if (!regex.IsMatch(item.ToString()))
+        {
+            errorMessage += item;
+        }
+    }
+    throw new SymbolException("Некоректные символы - " + errorMessage);
 }
 else
 {
-    inputString = Reverse(inputString) + inputString;
-}
+    int length = inputString.Length;
+    if (length % 2 == 0)
+    {
+        string firstHalf = inputString.Substring(0, length / 2);
+        string secondHalf = inputString.Substring(length / 2);
+        firstHalf = Reverse(firstHalf);
+        secondHalf = Reverse(secondHalf);
 
-Console.WriteLine(inputString);
+        inputString = firstHalf + secondHalf;
+    }
+    else
+    {
+        inputString = Reverse(inputString) + inputString;
+    }
+
+    Console.WriteLine(inputString);
+
+
+}
 
 static string Reverse(string s)
 {
     char[] charArray = s.ToCharArray();
     Array.Reverse(charArray);
     return new string(charArray);
+}
+class SymbolException : Exception
+{
+    public SymbolException(string message) : base(message) { }
 }
